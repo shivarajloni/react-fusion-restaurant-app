6265c1597773c4f469991d7466f1a0a6df197d9c
shivaraj loni <shivarajbloni@gmail.com>
2020-07-08 20:41:42 +0530

    added container component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './Components/MenuComponent';
+import Main from './Components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
-// import { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state ={
-      dishes: DISHES
-    };
-  }
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Resturant fusion</NavbarBrand> 
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
+       <Main />
     </div>
    );
  }
