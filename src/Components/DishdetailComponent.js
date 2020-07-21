6ac17b6622c0985279942c24f03b3e12efa889d9
shivaraj loni <shivarajbloni@gmail.com>
2020-07-22 00:45:42 +0530

    fetch from server using json-server

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len); 
 const minLength = (len) => (val) => !(val) && (val.length >= len);
@@ -11,7 +12,7 @@ const minLength = (len) => (val) => !(val) && (val.length >= len);
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
